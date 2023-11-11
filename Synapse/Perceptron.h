34796aa6abe7ebf0e6a7ba2a6a25288bc09f152c
Shivanshu Raj <rajshivanshu19@gmail.com>
2023-11-11 23:13:26 +0530

    added readme file

--- Synapse/Perceptron.h
+++ Synapse/Perceptron.h
@@ -16,7 +16,7 @@ namespace synp
 
 		perceptron(unsigned int input_neurons, unsigned int hidden_neurons, unsigned int output_neurons);
 		void train(Matf input, Matf target);
-		Matf predict(Matf input);
+		Matf infer(Matf input);
 
 
 	};
@@ -29,7 +29,7 @@ synp::perceptron::perceptron(unsigned int input_neurons, unsigned int hidden_neu
 
 	weight_layer1 = Matf(input_neurons, input_neurons, arma::fill::randn);
 	weight_layer2 = Matf(input_neurons, hidden_neurons, arma::fill::randn);
-
+	weight_layer3 = Matf(hidden_neurons, output_neurons, arma::fill::randn);
 }
 
 /// <summary>
@@ -44,22 +44,29 @@ void  synp::perceptron::train(Matf input,Matf target)
 	float tolerance = 0.1;
 	float learning_rate = 0.01;
 	signed int epoch = 1;
-	while (epoch < 10)
+
+
+	//input layer response
+
+	for (int sample = 0; sample < input.n_rows; sample++)
 	{
-		for (int sample = 0; sample < samples; sample++)
-		{
-			Matf response = predict(input.row(sample));
-			error = target.row(sample)[0] - response.row(0)[0];
-			printf("\n error: %f", error);
+		//assuming single neuron output in output layer
+		float response = infer(input.row(sample)).row(0)[0];
+		
+		float error = abs(response - target.row(sample)[0]);
 			if (error > tolerance)
 			{
-				weight_layer2 += learning_rate * (input.t() * target);
+				Matf sigma_sum = input * weight_layer1;
+				Matf learning_signal = synp::identity(sigma_sum);
+				for (int col_space = 0; col_space < weight_layer2.n_cols; col_space++)
+				{
+					weight_layer2.col(col_space) += learning_rate * (learning_signal * target.row(0)[0]);
+				}
 
 			}
+		
 
-		}
-		puts("\n\n");
-		epoch++;
+		
 	}
 
 
@@ -67,13 +74,17 @@ void  synp::perceptron::train(Matf input,Matf target)
 
 }
 
-Matf synp::perceptron::predict(Matf input)
+Matf synp::perceptron::infer(Matf input)
 {
 	Matf sigma_sum = input * weight_layer1;
-	Matf response = synp::bipolar(sigma_sum);
+	Matf response = synp::identity(sigma_sum);
 
 	sigma_sum = response * weight_layer2;
-	response = synp::bipolar(sigma_sum);
+	response = synp::binary(sigma_sum);
+
+
+	sigma_sum = response * weight_layer3;
+	response = synp::binary(sigma_sum);
 
 	return response;
 
--- Synapse/activators_mat.h
+++ Synapse/activators_mat.h
@@ -15,4 +15,17 @@ namespace synp
 		 ((*it) >= 0.0f ? 1.0f : -1.0f);
 		return X;
 	}
+	  Matf binary(Matf X)
+	  {
+
+		  Matf::iterator it = X.begin();
+		  Matf::iterator end = X.end();
+		  for (it; it != end; ++it)
+			  ((*it) >= 0.0f ? 1.0f : 0.0f);
+		  return X;
+	  }
+	  Matf identity(Matf X)
+	  {
+		  return X;
+	  }
 }
